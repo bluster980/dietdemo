ea3840e7acc36fd98b347b0bd37b1ffb9b5603b4
jatin <jatinparmar980@gmail.com>
2025-10-03 05:04:25 +0530

    fixed manifest file for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,60 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
-  plugins: [react(), svgr({ exportAsDefault: true, include: '**/*.svg' })],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-});
-
-
 // export default defineConfig({
-//   plugins: [
-//     react(),
-//     VitePWA({
-//       registerType: 'autoUpdate',
-//       manifest: {
-//         name: 'My Fitness App',
-//         short_name: 'FitnessApp',
-//         description: 'A fitness app built with React and Vite',
-//         theme_color: '#FFD98E',
-//         background_color: '#ffffff',
-//         display: 'standalone',
-//         scope: '/',
-//         start_url: '/',
-//         icons: [
-//           {
-//             src: '/icons/download48.png',
-//             sizes: '48x48',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/icons/download96.png',
-//             sizes: '96x96',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/icons/download192.png',
-//             sizes: '192x192',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/icons/download256.png',
-//             sizes: '256x256',
-//             type: 'image/png',
-//           },
-//           {
-//             src: '/icons/download512.png',
-//             sizes: '512x512',
-//             type: 'image/png',
-//           },
-//         ],
-//       },
-//     }),
-//   ],
+//   plugins: [react(), svgr({ exportAsDefault: true, include: '**/*.svg' })],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//   },
 // });
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({ exportAsDefault: true, include: '**/*.svg' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Diet Delta',
+        short_name: 'DietDelta',
+        description: 'A fitness app built for trainer and clients',
+        theme_color: '#FFFFFF',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/download48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/download96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/download192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/download256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/download512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
